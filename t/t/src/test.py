ea549fdab9177711d2d99640588c5f4db4c640cc
sbucher <bucher.scott@gmail.com>
2011-10-06 11:31:48 -0400

    testing first commit

---
+++ t/t/src/test.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 5, 2011
+
+@author: sbucher
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
